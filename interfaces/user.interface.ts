d561910984a5d926c3577e4f8fb2a5cb04aa3ce2
César Nóvoa <themotokar@gmail.com>
2021-06-20 18:29:55 +0200

    Update user.interface.ts

--- interfaces/user.interface.ts
+++ interfaces/user.interface.ts
@@ -2,13 +2,13 @@ export interface userCreationInterface {
     user: string;
     name: string;
     password: string;
+    repeatPassword: string;
     email: string;
 }
 
 export interface userUpdateInterface {
     user?: string;
     name?: string;
-    password?: string;
     email?: string;
     description?: string;
     site?: string;
